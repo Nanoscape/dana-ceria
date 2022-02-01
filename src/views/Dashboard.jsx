ff900d71c59ac04e1a52e49c1f5e15c40e48907a
Muhamad Arsyad Ramadhan <marsyad.ramadhan@unsircle.com>
2022-02-01 20:30:13 +0700

    save changes

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -11,29 +11,29 @@ export default class Dashboard extends Component {
             <a href="/#" className="nav__logo">
               <img src={LogoHeader} alt="Logo" className="nav__logoimg" />
             </a>
-            <ul class="nav__list">
-              <li class="nav__item">
-                <a href="/#" class="nav__link">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <a href="/#" className="nav__link">
                   Home
                 </a>
               </li>
-              <li class="nav__item">
-                <a href="/#" class="nav__link">
+              <li className="nav__item">
+                <a href="/#" className="nav__link">
                   Tentang Dana Ceria
                 </a>
               </li>
-              <li class="nav__item">
-                <a href="/#" class="nav__link">
+              <li className="nav__item">
+                <a href="/#" className="nav__link">
                   Produk Kami
                 </a>
               </li>
-              <li class="nav__item">
-                <a href="/#" class="nav__link">
+              <li className="nav__item">
+                <a href="/#" className="nav__link">
                   Mitra Kami
                 </a>
               </li>
-              <li class="nav__item">
-                <a href="/#" class="nav__link">
+              <li className="nav__item">
+                <a href="/#" className="nav__link">
                   Agent Dana Ceria
                 </a>
               </li>
