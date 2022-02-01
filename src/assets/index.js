6b10895240a449347a3898f74382ea6b08ffb333
Fachrul <fachrulfatahillah@gmail.com>
2022-02-01 21:27:26 +0700

    Add Section Hero

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import Logo from './images/logo.png';
 import LogoHeader from './images/logo-header.png';
+import BGHero from './images/bg-hero.png';
 
-export { Logo, LogoHeader };
+export { Logo, LogoHeader, BGHero };
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { LogoHeader } from '../assets';
+import { LogoHeader, BGHero } from '../assets';
 import '../assets/css/dashboard.css';
 
 export default class Dashboard extends Component {
@@ -11,28 +11,28 @@ export default class Dashboard extends Component {
             <a href="/#" className="nav__logo">
               <img src={LogoHeader} alt="Logo" className="nav__logoimg" />
             </a>
-            <ul class="nav__list">
+            <ul className="nav__list">
               <li class="nav__item">
                 <a href="/#" class="nav__link">
                   Home
                 </a>
               </li>
-              <li class="nav__item">
+              <li className="nav__item">
                 <a href="/#" class="nav__link">
                   Tentang Dana Ceria
                 </a>
               </li>
-              <li class="nav__item">
+              <li className="nav__item">
                 <a href="/#" class="nav__link">
                   Produk Kami
                 </a>
               </li>
-              <li class="nav__item">
+              <li className="nav__item">
                 <a href="/#" class="nav__link">
                   Mitra Kami
                 </a>
               </li>
-              <li class="nav__item">
+              <li className="nav__item">
                 <a href="/#" class="nav__link">
                   Agent Dana Ceria
                 </a>
@@ -40,7 +40,17 @@ export default class Dashboard extends Component {
             </ul>
           </nav>
         </header>
-        <div className="section__hero"></div>
+        <div className="section__hero">
+          <div className="desc-hero-wrapper">
+            <h2 className="desc-hero">
+              Platform <span className="desc-hero txt-yellow">Dana Pinjaman</span> Agunan BPKB & Kredit Mobil Baru/Bekas Mudah dan Cepat
+            </h2>
+            <h4 className="desc-down-hero">
+              Proses dibantu sampai <span className="desc-down-hero border-yellow">Disetujui</span> dan <span className="desc-down-hero border-yellow">Cair</span>
+            </h4>
+          </div>
+          <img src={BGHero} alt="Background Hero" className="img-hero" />
+        </div>
       </div>
     );
   }
