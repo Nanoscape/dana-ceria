6baf921b3dac2a63af3d35a10572668b4ef31c38
Fachrul <fachrulfatahillah@gmail.com>
2022-02-19 15:41:14 +0700

    Clean Before Deploy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,32 @@
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom';
+import React from 'react';
 import { LogoHeader } from '../assets';
 import '../assets/css/dashboard.css';
 
 export default function () {
-  const location = useLocation();
-
   return (
-    <header className="header">
-      <nav className="nav">
+    <header>
+      <nav>
         <a href="/#" className="nav__logo">
-          <img src={LogoHeader} alt="Logo" className="nav__logoimg" />
+          <img src={LogoHeader} alt="Logo" />
         </a>
-        <ul className="nav__list">
-          <li className="nav__item">
-            <Link to='/' className={`nav__link ${location.pathname === '/' ? 'active' : null}`}>Home</Link>
+        <ul>
+          <li>
+            <a href="/#">Home</a>
           </li>
-          <li className="nav__item">
-            <a href="/#" className="nav__link">
-              Tentang Dana Ceria
-            </a>
+          <li>
+            <a href="/#">Tentang Dana Ceria</a>
           </li>
-          <li className="nav__item">
-            <a href="/#" className="nav__link">
-              Produk Kami
-            </a>
+          <li>
+            <a href="/#">Produk Kami</a>
           </li>
-          <li className="nav__item">
-            <a href="/#" className="nav__link">
-              Mitra Kami
-            </a>
+          <li>
+            <a href="/#">Mitra Kami</a>
           </li>
-          <li className="nav__item">
-            <Link to='/agent' className={`nav__link ${location.pathname === '/agent' ? 'active' : null}`}>Agent Dana Ceria</Link>
+          <li>
+            <a href="/#">Agent Kami</a>
           </li>
         </ul>
       </nav>
     </header>
-  )
+  );
 }
